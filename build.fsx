a8f0ca3af13ad24d2c2b9c033776c1228729ac3a
saxonmatt <saxonmatt@gmail.com>
2013-10-23 22:44:28 +0100

    Grabbing a bit of build process from the scaffold.

---
+++ build.fsx
@@ -0,0 +1,32 @@
+﻿#r "packages/FAKE/tools/FakeLib.dll"
+open Fake
+open Fake.Git
+open Fake.AssemblyInfoFile
+open System
+
+// --------------------------------------------------------------------------------------
+// Static site tooling as a set of targets.
+// --------------------------------------------------------------------------------------
+Target "Generate" DoNothing
+
+Target "Preview" DoNothing
+
+Target "GenerateAndPreview" DoNothing
+
+Target "Deploy" DoNothing
+
+"Generate"
+    ==> "Preview"
+    ==> "GenerateAndPreview"
+
+// --------------------------------------------------------------------------------------
+// Build the tools as a separate set of targets.
+// --------------------------------------------------------------------------------------
+Target "Clean" DoNothing
+
+Target "Build" DoNothing
+
+// --------------------------------------------------------------------------------------
+// Run a specified target
+// --------------------------------------------------------------------------------------
+RunTargetOrDefault "GenerateAndPreview"
